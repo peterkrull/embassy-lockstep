0ea24f9d1853502d4b4729673f230e99f3a103cc
Peter Krull <peterkrullpeter@gmail.com>
2026-03-15 01:09:12 +0100

    test tasks should assert that they run at the initial tick.

--- src/tasks.rs
+++ src/tasks.rs
@@ -7,11 +7,12 @@ use embassy_time::{Duration, Instant, Timer};
 // and all other tasks are launched.
 #[embassy_executor::task]
 pub async fn firmware_main(spawner: Spawner) {
-    println!("Hello from main (ticks: {})", Instant::now().as_ticks());
+    let initial_tick = Instant::now().as_ticks();
+    println!("Hello from main (ticks: {initial_tick})");
 
-    spawner.must_spawn(task0());
-    spawner.must_spawn(task1());
-    spawner.must_spawn(task2());
+    spawner.must_spawn(task0(initial_tick));
+    spawner.must_spawn(task1(initial_tick));
+    spawner.must_spawn(task2(initial_tick));
 
     let start_time = Instant::now();
     while start_time.elapsed() < Duration::from_secs(2) {
@@ -24,8 +25,10 @@ pub async fn firmware_main(spawner: Spawner) {
 
 // Some task doing some time-dependent stuff
 #[embassy_executor::task]
-pub async fn task0() {
-    println!("Hello from task0 (ticks: {})", Instant::now().as_ticks());
+pub async fn task0(intial_tick: u64) {
+    let ticks = Instant::now().as_ticks();
+    println!("Hello from task0 (ticks: {ticks})");
+    assert_eq!(ticks, intial_tick);
 
     Timer::after_millis(100).await;
 
@@ -44,8 +47,10 @@ pub async fn task0() {
 
 // Some task doing some time-dependent stuff
 #[embassy_executor::task]
-pub async fn task1() {
-    println!("Hello from task1 (ticks: {})", Instant::now().as_ticks());
+pub async fn task1(intial_tick: u64) {
+    let ticks = Instant::now().as_ticks();
+    println!("Hello from task1 (ticks: {ticks})");
+    assert_eq!(ticks, intial_tick);
 
     Timer::after_millis(110).await;
 
@@ -60,8 +65,10 @@ pub async fn task1() {
 
 // Some task doing some time-dependent stuff
 #[embassy_executor::task]
-pub async fn task2() {
-    println!("Hello from task2 (ticks: {})", Instant::now().as_ticks());
+pub async fn task2(intial_tick: u64) {
+    let ticks = Instant::now().as_ticks();
+    println!("Hello from task2 (ticks: {ticks})");
+    assert_eq!(ticks, intial_tick);
 
     Timer::after_millis(120).await;
 
